b2e7ce182ebf88a35a5a3162f22d009833258c1e
Xolve <xolve@protonmail.com>
2020-09-05 20:31:41 +0530

    Use metals for scala.

--- init.d/init-lang-scala.el
+++ init.d/init-lang-scala.el
@@ -7,6 +7,21 @@
 
 (add-hook 'scala-mode-hook #'lsp-deferred)
 
-(use-package sbt-mode :ensure t)
+(use-package lsp-metals :ensure t)
+
+;; Enable sbt mode for executing sbt commands
+(use-package sbt-mode :ensure t
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+
+   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+   (setq sbt:program-options '("-Dsbt.supershell=false"))
+)
 
 (provide 'init-lang-scala)
